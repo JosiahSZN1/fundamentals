a110ba29d18ab26fa95614c20541c57c953c6b3b
Levi Josiah Mariano <lejosiahm@gmail.com>
2023-03-09 16:09:10 -0800

    test

---
+++ assignments/cafe_business_logic/CafeUtil.java
@@ -0,0 +1,17 @@
+class CafeUtil {
+    public int getStreakGoal() {
+        for(int i =)
+    }
+
+    public double getOrderTotal(double[] prices) {
+
+    }
+
+    public void displayMenu(ArrayList<String> menuItems) {
+
+    }
+
+    public void addCustomer(ArrayList<String> customers) {
+
+    }
+}
