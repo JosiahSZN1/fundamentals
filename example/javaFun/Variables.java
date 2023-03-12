d9a97bdc23f2d5a00aba93d8ffc749489815a81c
Levi Josiah Mariano <lejosiahm@gmail.com>
2023-03-12 12:45:45 -0700

    Reviewed Variables lesson, added some example code

--- example/javaFun/Variables.java
+++ example/javaFun/Variables.java
@@ -1,5 +1,17 @@
 public class Variables {
     public static void main(String[] args){
+        
+        String firstName = "Kurt";
+
+        firstName = "Vonnegut";
+
+        double price1 = 4;
+        double price2 = price1;
+
+        double total = 0;
+        total = price1 + price2;
+        System.out.println(total);
+
         int ourInt; // we can declare a variable without setting its value
         ourInt = 400; // we can assign a value to the variable later in our code
         double pi = 3.14159265; // we can also declare and assign on the same line
